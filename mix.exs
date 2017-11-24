2dc7d931e8ab8884bda85607edef67683480d7d8
Aldric Giacomoni <trevoke@gmail.com>
2017-11-24 18:15:19 -0500

    add some fields to mix.exs for release

--- mix.exs
+++ mix.exs
@@ -2,12 +2,18 @@ defmodule Ecstatic.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ecstatic,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :ecstatic,
+      version: "0.0.1",
+      elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: "",
+      licenses: ["MIT"],
+      maintainers: ["Aldric Giacomoni"],
+      links: "https://github.com/trevoke/ecstatic"
+    ]
   end
 
   # Configuration for the OTP application
