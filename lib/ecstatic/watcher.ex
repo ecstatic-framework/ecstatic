04e6f4cf3bba63e9c16e808733fb6f04cff328be
Aldric Giacomoni <trevoke@gmail.com>
2017-12-02 23:48:05 -0500

    I generate AST. How to get it back to code?

--- lib/ecstatic/watcher.ex
+++ lib/ecstatic/watcher.ex
@@ -28,24 +28,40 @@ defmodule Ecstatic.Watcher do
   defmacro __using__(_options) do
     quote do
       import unquote(__MODULE__)
-      Module.register_attribute(__MODULE__, :watchers, accumulate: true)
+      Module.register_attribute(__MODULE__, :watchers, [])
+      Module.put_attribute(__MODULE__, :watchers, [])
       @before_compile unquote(__MODULE__)
     end
   end
-  defmacro __before_compile__(_env) do
+  defmacro __before_compile__(env) do
+    x = Enum.map(
+      Module.get_attribute(env.module, :watchers),
+      fn(watcher) ->
+        Map.update!(
+          watcher, :callback,
+          fn(callback) ->
+            IO.inspect(quote do unquote(callback) end)
+          end)
+    end)
+    Module.put_attribute(
+      env.module,
+      :watchers,
+      x)
     quote do
       def watchers, do: @watchers
     end
   end
 
   defmacro watch_component(comp, hook, callback, system) do
-    quote do
-      @watchers %{
-        component: unquote(comp),
-        hook: unquote(hook),
-        callback: callback,
-        system: unquote(system)
-      }
+    x = Macro.escape(callback)
+    quote location: :keep do
+      Module.put_attribute(__MODULE__, :watchers, [
+            %{
+              component: unquote(comp),
+              hook: unquote(hook),
+              callback: unquote(x),
+              system: unquote(system)
+            } | Module.get_attribute(__MODULE__, :watchers)])
     end
   end
 end
