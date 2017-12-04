d8c89ceb19a66a52ce51c56b85b46672afcdd9cb
Aldric Giacomoni <trevoke@gmail.com>
2017-12-03 22:18:14 -0500

    clean still broken

--- lib/ecstatic/watcher.ex
+++ lib/ecstatic/watcher.ex
@@ -1,66 +1,30 @@
 defmodule Ecstatic.Watcher do
-  alias Ecstatic.Component, as: C
-
-  defstruct [
-    :component_type,
-    :trigger,
-    :system
-  ]
-
-  @type component_type :: atom()
-
-  @type trigger :: [
-    state: (C -> boolean())
-  ] | [
-    change: (C, C -> boolean())
-  ] | [
-    every: {integer(), :second | :seconds | :minute | :minutes | :hour | :hours}
-  ]
-
-  @type system :: atom()
-
-  @type t :: %__MODULE__{
-    component_type: atom(),
-    trigger: trigger(),
-    system: atom()
-  }
-
   defmacro __using__(_options) do
     quote do
       import unquote(__MODULE__)
-      Module.register_attribute(__MODULE__, :watchers, [])
-      Module.put_attribute(__MODULE__, :watchers, [])
+      Module.register_attribute(__MODULE__, :watchers, [accumulate: true])
       @before_compile unquote(__MODULE__)
     end
   end
   defmacro __before_compile__(env) do
-    watchers = Module.delete_attribute(env.module, :watchers)
-
-    foo =
-      watchers
-      |> Enum.map(fn(watcher) -> Map.update!(watcher, :callback, &(Code.eval_quoted(&1) |> elem(0))) end)
-
     quote location: :keep do
       def watchers do
-        unquote(foo |> IO.inspect)
+        @watchers
       end
     end
   end
 
   defmacro watch_component(comp, hook, callback, system) do
-    x = Macro.escape(callback)
-    map = quote do %{
-      component: unquote(comp),
-      hook: unquote(hook),
-      callback: unquote(x),
-      system: unquote(system)
-    }
+    map = quote do
+      %{
+        component: unquote(comp),
+        hook: unquote(hook),
+        callback: unquote(callback),
+        system: unquote(system)
+      }
     end
-    module = __CALLER__.module
     quote location: :keep do
-      Module.put_attribute(unquote(module), :watchers, [
-            unquote(map) | Module.get_attribute(unquote(module), :watchers)
-          ])
+      @watchers unquote(map)
     end
   end
 end
--- lib/ecstatic/watchers.ex
+++ lib/ecstatic/watchers.ex
@@ -1,4 +1,5 @@
 defmodule Ecstatic.Watchers do
   use Ecstatic.Watcher
   watch_component(Age, :attach, fn(_e, _c) -> true end, StartAgeTick)
+  watch_component(Age, :detach, fn(_e, _c) -> false end, StopAgeTick)
 end
