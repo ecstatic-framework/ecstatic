0123eb5e35a20be10adeb364a38a2c86c6e61ad6
Aldric Giacomoni <trevoke@gmail.com>
2017-12-22 22:28:14 -0500

    use match_aspect correctly

--- lib/ecstatic/system.ex
+++ lib/ecstatic/system.ex
@@ -2,14 +2,14 @@ defmodule Ecstatic.System do
   @callback aspect() :: Aspect.t
   @callback dispatch(entity :: Ecstatic.Entity.t) :: Ecstatic.Changes.t
   defmacro __using__(_options) do
-    quote do
-      @behaviour Ecs.System
-      alias Ecstatic.{Aspect, Changes, Component, Entity}
+    quote location: :keep do
+      @behaviour Ecstatic.System
+      alias Ecstatic.{Aspect, Changes, Component, Entity, EventQueue}
 
       @spec process(entity :: Entity.t) :: :ok
       def process(entity) do
         # TODO move this match_aspect? to the watcher definition?
-        event = if Entity.match_aspect?(aspect(), entity) do
+        event = if Entity.match_aspect?(entity, aspect()) do
           {entity, dispatch(entity)}
         else
           {entity, %Changes{}}
