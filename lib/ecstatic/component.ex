21cef0769672ef804e478b31b1861795b4a91ebb
Aldric Giacomoni <trevoke@gmail.com>
2017-12-22 22:30:45 -0500

    components use a module attribute for default value

--- lib/ecstatic/component.ex
+++ lib/ecstatic/component.ex
@@ -12,15 +12,21 @@ defmodule Ecstatic.Component do
     id: id
   }
 
-  @callback default_value :: map()
-
   defmacro __using__(_options) do
-    quote do
-      @behaviour Component
+    quote location: :keep do
+      Module.register_attribute(__MODULE__, :default_value, [])
+      import unquote(__MODULE__)
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    quote location: :keep do
+      @default_value @default_value || %{}
       def new(initial_state \\ %{}) do
         Component.new(
           __MODULE__,
-          Map.merge(default_value(), initial_state)
+          Map.merge(@default_value, initial_state)
         )
       end
     end
