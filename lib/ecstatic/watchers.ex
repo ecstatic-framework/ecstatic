bb406ed502a046e3d9bd326ce70cd6dd4d5925d8
Aldric Giacomoni <trevoke@gmail.com>
2018-01-04 22:09:38 -0500

    remove unnecessary sample file

--- lib/ecstatic/watchers.ex
+++
@@ -1,5 +0,0 @@
-defmodule Ecstatic.Watchers do
-  use Ecstatic.Watcher
-  watch_component(Age, :attached, fn(_e, _c) -> true end, StartAgeTick)
-  watch_component(Age, :removed, fn(_e, _c) -> false end, StopAgeTick)
-end
