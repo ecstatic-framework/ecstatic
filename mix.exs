a427ad32aa6d01e542d92dfcb385a4d43178d9a8
Aldric Giacomoni <trevoke@gmail.com>
2018-01-04 22:07:06 -0500

    0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ecstatic.Mixfile do
   def project do
     [
       app: :ecstatic,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
