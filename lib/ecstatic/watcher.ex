d12320c58e72fe05eabc9d79d7ce67f5c9ba085f
Aldric Giacomoni <trevoke@gmail.com>
2019-02-02 21:25:22 -0500

    steps towards a better watcher DSL

--- lib/ecstatic/watcher.ex
+++ lib/ecstatic/watcher.ex
@@ -1,7 +1,7 @@
 defmodule Ecstatic.Watcher do
   @doc false
   defmacro __using__(_options) do
-    quote location: :keep do
+    quote do
       Module.register_attribute(__MODULE__, :watchers, accumulate: true)
       import unquote(__MODULE__)
       @before_compile unquote(__MODULE__)
@@ -9,11 +9,10 @@ defmodule Ecstatic.Watcher do
   end
 
   @doc false
-  defmacro __before_compile__(_env) do
-    quote location: :keep do
-      @watchers
+  defmacro __before_compile__(env) do
+    quote do
       def watchers do
-        Code.eval_quoted @watchers
+        unquote(Module.get_attribute(env.module, :watchers))
       end
     end
   end
@@ -27,96 +26,72 @@ defmodule Ecstatic.Watcher do
   end
 
   defmacro run(system, when: predicate) do
-    x =
-      quote location: :keep do
-        Module.get_attribute(__MODULE__, :current_component)
-      end
-
-    y =
-      quote location: :keep do
-        if unquote(x) == nil do
-          raise "run/2 cannot be called outside a watch/2 macro code block"
-        end
-
-        watch_component(unquote(x), run: unquote(system), when: unquote(predicate))
-      end
-
-
-  end
-
-  defmacro run(system, every: milliseconds) do
-    x =
-      quote location: :keep do
-        Module.get_attribute(__MODULE__, :current_component)
-      end
-
-    y =
-      quote location: :keep do
-        if unquote(x) == nil do
-          raise "run/2 cannot be called outside a watch/2 macro code block"
-        end
-
-        watch_component(unquote(x), run: unquote(system), every: unquote(milliseconds))
-      end
-
-    Macro.expand(y, __ENV__)
-  end
-
-  defmacro watch_component(comp, run: system, when: predicate) do
     map =
-      quote location: :keep do
+      quote do
         %{
-          component: unquote(comp),
-          component_lifecycle_hook: :updated,
           callback: unquote(predicate),
+          component: @current_component,
+          component_lifecycle_hook: :updated,
           system: unquote(system)
         }
       end
 
-    quote location: :keep do
+    quote do
       @watchers unquote(Macro.escape(map))
-    end
-  end
-
-  defmacro watch_component(comp, run: system, every: milliseconds) do
-    map =
-      quote location: :keep do
-        start_tick = fn _e, c ->
-          Process.send_after(
-            self(),
-            {:tick, c.id, unquote(system), unquote(milliseconds)},
-            unquote(milliseconds)
-          )
-
-          true
-        end
-
-        %{
-          component: unquote(comp),
-          component_lifecycle_hook: :attached,
-          callback: start_tick,
-          system: Ecstatic.NullSystem
-        }
-      end
-
-    map2 =
-      quote location: :keep do
-        stop_tick = fn _e, c ->
-          Process.send_after(self(), {:stop_tick, c.id}, 20)
-          true
-        end
-
-        %{
-          component: unquote(comp),
-          component_lifecycle_hook: :removed,
-          callback: stop_tick,
-          system: Ecstatic.NullSystem
-        }
-      end
-
-    quote location: :keep do
-      @watchers unquote(Macro.escape(map))
-      @watchers unquote(Macro.escape(map2))
     end
   end
 end
+
+# defmacro run(system, every: milliseconds) do
+#   map =
+#     quote location: :keep do
+#       component = Module.get_attribute(__MODULE__, :current_component)
+
+#       if component == nil do
+#         raise "run/2 cannot be called outside a watch/2 macro code block"
+#       end
+
+#       start_tick = fn _e, c ->
+#         Process.send_after(
+#           self(),
+#           {:tick, c.id, unquote(system), unquote(milliseconds)},
+#           unquote(milliseconds)
+#         )
+
+#         true
+#       end
+
+#       %{
+#         component: component,
+#         component_lifecycle_hook: :attached,
+#         callback: start_tick,
+#         system: Ecstatic.NullSystem
+#       }
+#     end
+
+#   map2 =
+#     quote location: :keep do
+#       component = Module.get_attribute(__MODULE__, :current_component)
+
+#       if component == nil do
+#         raise "run/2 cannot be called outside a watch/2 macro code block"
+#       end
+
+#       stop_tick = fn _e, c ->
+#         Process.send_after(self(), {:stop_tick, c.id}, 20)
+#         true
+#       end
+
+#       %{
+#         component: component,
+#         component_lifecycle_hook: :removed,
+#         callback: stop_tick,
+#         system: Ecstatic.NullSystem
+#       }
+#     end
+
+#   quote location: :keep, bind_quoted: [map: map, map2: map2] do
+#     @watchers unquote(Macro.escape(map))
+#     @watchers unquote(Macro.escape(map2))
+#   end
+# end
