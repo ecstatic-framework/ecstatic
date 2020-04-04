58bbd59863388cb0d9e6de8775bfe4336b61da2f
Alexander Pilafian <sikanrong@gmail.com>
2020-04-04 20:45:52 +0100

    Readding the module notation to Ecstatic.Store module - somehow only the content of this module remained after last commit

--- lib/ecstatic/store.ex
+++ lib/ecstatic/store.ex
@@ -1,6 +1,9 @@
+defmodule Ecstatic.Store do 
   alias Ecstatic.Entity
+
   @type return_type :: {:ok, Entity.t()} | {:error, term()}
   @type entity_id_type :: pos_integer() | String.t()
   @callback save_entity(Entity.t()) :: return_type
   @callback get_entity(entity_id_type) :: return_type
   @callback delete_entity(entity_id_type) :: none()
+end
